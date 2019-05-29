ec64f47e5e00261d0f77834b07e836c6bd5d8b8c
osuhomebase <mbaron999@gmail.com>
2019-05-29 11:17:19 -0400

    initial commit

---
+++ MoveFiles/MoveFiles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MoveFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\SourceCode\MoveFiles\SampleSource\";
+            string destinationPath = @"C:\SourceCode\MoveFiles\SampleDestination\";
+
+
+            for(int i = 0; i < 10; i++)
+            {
+                Regex reg = new Regex(@"\S*" + i.ToString() + ".jpg$");
+                var files = Directory.GetFiles(sourcePath, "*.jpg")
+                     .Where(path => reg.IsMatch(path))
+                     .ToList();
+                for(int j=0; j<=10; j++)
+                {
+                    IEnumerable<string> subFiles = files.Where(f => f.EndsWith(j.ToString() + i.ToString() + ".jpg"));
+
+                    foreach (string f in subFiles)
+                    {
+                        string sourceFile = sourcePath + Path.GetFileName(f);
+                        string destinationFile = destinationPath + i.ToString() + @"\" + Path.GetFileName(f);
+                        File.Move(sourceFile, destinationFile);
+                        Console.WriteLine(f);
+                    }
+                }
+            }
+            var x = Console.ReadKey();
+        }
+    }
+}
