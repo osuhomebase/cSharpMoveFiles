488332d0e6e73c251ed6dbed6c10b0e16e7b5859
osuhomebase <mbaron999@gmail.com>
2019-06-03 15:16:16 -0400

    made Extract class public.  Fixed the sign on constructor set method

--- MoveFiles/MoveFiles/Extract.cs
+++ MoveFiles/MoveFiles/Extract.cs
@@ -14,7 +14,7 @@ using SharpCompress.Readers.Tar;
 
 namespace MoveFiles
 {
-    class Extract
+    public class Extract
     {
         private string _sourceDirectory;
         private string _destinationDirectory;
@@ -23,9 +23,8 @@ namespace MoveFiles
             get { return _sourceDirectory; }
             set {
                 DirectoryInfo dir = new DirectoryInfo(Path.GetFullPath(value));
-                if (dir.Exists)
-                    throw new ArgumentException($"{nameof(value)} must be a valid directory. C'mon man!");
-               
+                if (!dir.Exists)
+                    throw new ArgumentException($"{nameof(value)} must be a valid directory. C'mon man!");              
                 _sourceDirectory = value;
             }
         }
@@ -35,50 +34,54 @@ namespace MoveFiles
             set
             {
                 DirectoryInfo dir = new DirectoryInfo(Path.GetFullPath(value));
-                if (dir.Exists)
+                if (!dir.Exists)
                     throw new ArgumentException($"{nameof(value)} must be a valid directory. C'mon man!");
                 _destinationDirectory = value;
             }
         }
 
-
+        public Extract(string SourceDirectory, string DestinationDirectory)
+        {
+            sourceDirectory = SourceDirectory;
+            destinationDirectory = DestinationDirectory;
+        }
 
         public Extract()
         {
-            using (var stream = File.OpenRead(@"T:\Photos\Import\Imported\housingphotos.full.20190525.tgz"))
-            using (var archive = ArchiveFactory.Open(stream))
-            {
-                var entry = archive.Entries.First();
-                entry.WriteToFile(Path.Combine(@"T:\Photos\Import\Imported\", entry.Key));
-            }
-            using (var stream = File.OpenRead(@"T:\Photos\Import\Imported\housingphotos.full.tar"))
-            using (var reader = TarReader.Open(stream))
-            {
-                int i = 0;
-                while (reader.MoveToNextEntry() && i < 10)
-                {
-                    if (!reader.Entry.IsDirectory)
-                    {
-                        using (var entryStream = reader.OpenEntryStream())
-                        {
-                            string file = Path.GetFileName(reader.Entry.Key);
-                            string folder = Path.GetDirectoryName(reader.Entry.Key);
-                            string destdir = @"T:\Photos\Import\Imported\";
-                            if (!Directory.Exists(destdir))
-                            {
-                                Directory.CreateDirectory(destdir);
-                            }
-                            string destinationFileName = Path.Combine(destdir, file);
+            //using (var stream = File.OpenRead(@"T:\Photos\Import\Imported\housingphotos.full.20190525.tgz"))
+            //using (var archive = ArchiveFactory.Open(stream))
+            //{
+            //    var entry = archive.Entries.First();
+            //    entry.WriteToFile(Path.Combine(@"T:\Photos\Import\Imported\", entry.Key));
+            //}
+            //using (var stream = File.OpenRead(@"T:\Photos\Import\Imported\housingphotos.full.tar"))
+            //using (var reader = TarReader.Open(stream))
+            //{
+            //    int i = 0;
+            //    while (reader.MoveToNextEntry() && i < 10)
+            //    {
+            //        if (!reader.Entry.IsDirectory)
+            //        {
+            //            using (var entryStream = reader.OpenEntryStream())
+            //            {
+            //                string file = Path.GetFileName(reader.Entry.Key);
+            //                string folder = Path.GetDirectoryName(reader.Entry.Key);
+            //                string destdir = @"T:\Photos\Import\Imported\";
+            //                if (!Directory.Exists(destdir))
+            //                {
+            //                    Directory.CreateDirectory(destdir);
+            //                }
+            //                string destinationFileName = Path.Combine(destdir, file);
 
-                            using (FileStream fs = File.OpenWrite(destinationFileName))
-                            {
-                                entryStream.CopyTo(fs);
-                            }
-                        }
-                    }
-                    i++;
-                }
-            }
+            //                using (FileStream fs = File.OpenWrite(destinationFileName))
+            //                {
+            //                    entryStream.CopyTo(fs);
+            //                }
+            //            }
+            //        }
+            //        i++;
+            //    }
+            //}
         }
     }
 }
